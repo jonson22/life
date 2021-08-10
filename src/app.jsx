a96f56c21ae28f0c2b2cdc33a52af849db534ebc
jonson22 <jonatonstiff@gmail.com>
2021-08-11 00:14:38 +0900

    app jsx created

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+    constructor(props){
+    super(props);
+    this.state = {
+        title: 'Hello World',
+        count: 0,
+    };
+    }
     render() {
-        let title = 'hello world';
-        const change = ()=>{
-            title = 'good bye';
-            console.log(title);
+    
+        const change = () => { 
+           this.setState({ title: 'World' });
 
         };
+        console.log(this);
         return(
 
             <div>
-                <h1>{title}</h1>
-                <button onClik={change}>change</button>
+                <h1>{this.state.title}</h1>
+               <button onClik={change}>change</button>
             </div>
 
       );  
